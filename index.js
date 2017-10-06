4ac3c9f7b2f4666829d11fcea1d5461253bf43bb
James <james@smoothbook.co>
2017-10-06 15:25:41 +0700

    Use commonjs in index.js for npm

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 
-// var client = require('./src/MixClient'),
-//     connector = require('./src/MixConnector');
-
-import MixClient from './src/MixClient.js';
-import MixConnector from './src/MixConnector.js';
+var client = require('./src/MixClient'),
+    connector = require('./src/MixConnector');
 
 module.exports = {
     MixClient : client.default,
